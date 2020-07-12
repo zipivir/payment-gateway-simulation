19cc3380633681e620d5f91e837fe4203e92b451
Noam Horovitz <42001923+noam-horovitz@users.noreply.github.com>
2020-07-12 18:14:26 +0300

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+
+namespace RiskifiedServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8000/");
+            listener.Start();
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                // Construct a response.
+                string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+
+                output.Close();
+            }
+
+            listener.Stop();
+        }
+    }
+}
